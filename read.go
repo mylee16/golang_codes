880ba84820f4559ed6b9edf5381618a954eb8fe0
Mengyong Lee <meng_yong_lee@aiap.sg>
2021-02-16 23:06:48 +0800

    added read.go

--- read.go
+++ read.go
@@ -14,39 +14,61 @@ from the file, your program should iterate through your slice of structs and pri
 names found in each struct.
 */
 
+// https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
+// https://stackoverflow.com/questions/26159416/init-array-of-structs-in-go
+
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 
-	var name string
-	fmt.Printf("Please enter your name: ")
-	fmt.Scan(&name)
+	var filename string
+	fmt.Printf("Please enter file name: ")
+	fmt.Scan(&filename)
 
-	var address string
-	fmt.Printf("Please enter your address: ")
-	fmt.Scan(&address)
-
-	type Person struct {
-		Name    string
-		Address string
+	type Name struct {
+		Fname string
+		Lname string
 	}
 
-	p1 := Person{Name: name,
-		Address: address}
+	nameStruct := []Name{}
 
-	// convert to byte array
-	barr, err := json.Marshal(p1)
+	file, err := os.Open(filename)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(string(barr))
+	reader := bufio.NewReader(file)
 
+	var line string
+	// saving to slice of struct
+	for {
+		line, err = reader.ReadString('\n')
+		line = strings.TrimSuffix(line, "\n")
+		s := strings.Split(line, " ")
+
+		p1 := Name{Fname: s[0],
+			Lname: s[1]}
+
+		nameStruct = append(nameStruct, p1)
+
+		if err != nil {
+			break
+		}
+	}
+
+	// iterate over nameStruct to print out first and last name
+	for _, names := range nameStruct {
+		fmt.Println("First name:", names.Fname)
+		fmt.Println("Last Name:", names.Lname)
+		fmt.Println("")
+	}
 }
