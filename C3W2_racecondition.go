1b6c5a93ae7918d597554daf4eab8f56cbfc839a
Mengyong Lee <meng_yong_lee@aiap.sg>
2021-04-10 22:26:37 +0800

    added race condition

---
+++ C3W2_racecondition.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+/*
+Raca condition is when multiple threads are trying to access and manipulat the same variable.
+In the code below, main, add_one and sub_one are all accessing and changing the value of x.
+Due to the uncertainty of Goroutine scheduling mechanism, the results of the following program is unpredictable.
+*/
+
+
+func add_one(num *int) {
+	for z := 0; z < 50; z++ {
+		(*num)++
+		fmt.Println("plus", *num)
+	}
+}
+
+func sub_one(num *int) {
+	for z := 0; z < 50; z++ {
+		(*num)--
+		fmt.Println("minus", *num)
+	}
+}
+
+func main() {
+  i := 0
+
+  go add_one(&i)
+  go sub_one(&i)
+
+  time.Sleep(time.Second)
+  fmt.Println("end")
+
+}
